7b44e20525812fff44511a8ad11ba4517f55e5e5
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-05-01 15:13:25 -0300

    feat adding dashboard page component

---
+++ src/app/features/dashboard/pages/dashboard-page.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.html',
+  standalone: true
+})
+export class DashboardPage {}
