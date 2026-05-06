6fc17d77a9226f39bcb64c2d233a42228191d36f
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-05-05 23:32:08 -0300

    feat: adding user list page

---
+++ src/app/features/user/pages/user-list-page/user-list.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+import { UserTableComponent } from '../../components/user-table/user-table.component'
+import { HeaderComponent } from '../../../../shared/components/header/header.component'
+
+
+@Component({
+  selector: 'app-user-list',
+  imports: [HeaderComponent, UserTableComponent],
+  templateUrl: './user-list.page.html',
+  styleUrl: './user-list.page.scss',
+  standalone: true
+})
+export class UserListPage {
+}
