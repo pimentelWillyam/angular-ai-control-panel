3fee8a082609572bdea885134e9b118435cbeab5
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-05-10 05:44:54 -0300

    feat: adding login page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router'
 import { UserListPage } from './features/list-users/pages/user-list-page/user-list.page'
+import { LoginPage } from './features/auth/pages/login.page'
 
 export const routes: Routes = [
-    {path: '', title: 'User List', component: UserListPage}
+    {path: '', title: 'Login', component: LoginPage},
+    {path: 'users', title: 'User List', component: UserListPage},
 ]
