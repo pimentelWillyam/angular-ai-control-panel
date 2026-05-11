1840036c5e50cd840ce112c87db28433514e5154
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-05-11 08:01:02 -0300

    feat: adding edit user table scss and ts

---
+++ src/app/features/edit-user/components/edit-user-table/edit-user-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-edit-user-table',
+    imports: [],
+    templateUrl: 'edit-user-table.component.html',
+    styleUrl: 'edit-user-table.component.scss',
+    standalone: true
+})
+
+export class EditUserTableComponent {
+
+}
