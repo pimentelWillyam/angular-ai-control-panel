80218eb205b7e2c52357a13e34c8b72384cc867e
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-05-11 09:51:36 -0300

    feat: adding login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { EditUserPage } from './features/edit-user/pages/edit-user.page'
 import { LoginPage } from './features/auth/pages/login.page'
 
 export const routes: Routes = [
-    {path: '', title: 'User List', component: UserListPage},
-    {path: 'edit-user/:id', title: 'Edit User', component: EditUserPage},
+    {path: '', title: 'Login', component: LoginPage},
     {path: 'users', title: 'User List', component: UserListPage},
+    {path: 'edit-user/:id', title: 'Edit User', component: EditUserPage},
+
 
 ]
