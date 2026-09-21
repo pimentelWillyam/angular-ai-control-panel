f4ee76a760ad5fe9effda1c2fe7f5c32cc933139
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-09-21 14:57:44 -0300

    feat: adding edit user modal

---
+++ src/app/features/edit-user/components/edit-user-modal/edit-user-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core"
+import { User } from "../../../../shared/models/User.model"
+
+@Component({
+    selector: 'app-edit-user-modal',
+    imports: [Component],
+    templateUrl: './edit-user-modal.component.html',
+    styleUrl: './edit-user-modal.component.scss',
+    standalone: true
+})
+
+export class EditUserModalComponent {
+
+    private openModal(user: User) {
+        console.log('Open modal for user:', user)
+    }
+
+    private closeModal() {
+        console.log('Close modal')
+    }
+
+}
