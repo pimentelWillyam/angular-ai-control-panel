5c71487348c9573506a5a73a6f0c10c3f8d1dff8
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-09-25 16:33:40 -0300

    feat: adding modal to list user table

--- src/app/features/list-users/components/tables/user-table/list-user-table.component.ts
+++ src/app/features/list-users/components/tables/user-table/list-user-table.component.ts
@@ -1,35 +1,42 @@
-import { Component, inject, OnInit } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { MatTableModule } from '@angular/material/table'
-import { MatButtonModule } from "@angular/material/button"
-import { User } from "../../../../../shared/models/User.model"
-import { HttpClient } from "@angular/common/http"
-import {MatTableDataSource} from "@angular/material/table";
-import { EditUserModalComponent } from "../../../../edit-user/components/edit-user-modal/edit-user-modal.component"
+import { Component, inject, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { HttpClient } from "@angular/common/http";
+import { MatTableDataSource } from "@angular/material/table";
+
+import { User } from "../../../../../shared/models/User.model";
+import { EditUserDialogComponent } from "../../../../edit-user/components/edit-user-modal/edit-user-dialog.component";
 
 @Component({
     selector: 'app-list-user-table',
-    imports: [CommonModule, EditUserModalComponent, MatTableModule, MatButtonModule],
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatTableModule,
+        MatButtonModule,
+        MatDialogModule
+    ],
     templateUrl: './list-user-table.component.html',
     styleUrl: './list-user-table.component.scss',
-    standalone: true
 })
-
 export class ListUserTableComponent implements OnInit {
-    dataSource = new MatTableDataSource<User>()
 
-    ngOnInit(){
-        this.fetchUsers()
+    dataSource = new MatTableDataSource<User>();
+
+    private http = inject(HttpClient);
+    private dialog = inject(MatDialog);
+
+    ngOnInit() {
+        this.fetchUsers();
     }
 
-    private http = inject(HttpClient)
-
     private fetchUsers() {
-
         this.http.get<User[]>('http://localhost:3000/user').subscribe({
             next: (response) => {
                 console.log('Users fetched successfully:', response);
-                this.dataSource.data = response
+                this.dataSource.data = response;
             },
             error: (error) => {
                 console.error('Failed to fetch users:', error);
@@ -38,26 +45,46 @@ export class ListUserTableComponent implements OnInit {
         });
     }
 
-
     editUser(user: User) {
-        console.log('Editing user....')
-        if (this.dataSource.data.findIndex(u => u.id === user.id) === -1) {
-            console.log('User not found')
-            alert('User not found')
-            return
-        }
-        console.log('Editing user:', user)
+        const userExists = this.dataSource.data.some(
+            u => u.id === user.id
+        );
 
+        if (!userExists) {
+            console.log('User not found');
+            alert('User not found');
+            return;
+        }
+
+        const dialogRef = this.dialog.open(EditUserDialogComponent, {
+            width: '500px',
+            data: user
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                console.log('Dialog result:', result);
+
+                this.dataSource.data = this.dataSource.data.map(u =>
+                    u.id === result.id ? result : u
+                );
+            }
+        });
     }
 
     removeUser(user: User) {
-        if (this.dataSource.data.findIndex(u => u.id === user.id) === -1) {
-            console.log('User not found')
-            alert('User not found')
-            return
+        const userExists = this.dataSource.data.some(
+            u => u.id === user.id
+        );
+
+        if (!userExists) {
+            console.log('User not found');
+            alert('User not found');
+            return;
         }
-        this.dataSource.data = this.dataSource.data.filter(u => u.id !== user.id)
+
+        this.dataSource.data = this.dataSource.data.filter(
+            u => u.id !== user.id
+        );
     }
-
-
 }
